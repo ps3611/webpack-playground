70285a66a436aea0379373c39e0d372a828598c6
Peter Schindler <petertasschindler@gmail.com>
2019-01-17 18:59:12 +0100

    feat: loading files

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
         use: ['style-loader', 'css-loader']
       },
       {
-        test:,/\(png|svg|jpg|gif)$/,
+        test: /\.(png|svg|jpg|gif)$/,
         use: ['file-loader']
       }
     ]
