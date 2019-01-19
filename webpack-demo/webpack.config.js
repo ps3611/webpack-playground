2b6c271ec3a6cb9b4cc60e4dbe2aaf5aa7a4672a
Peter Schindler <petertasschindler@gmail.com>
2019-01-19 13:05:05 +0100

    chore: setting up dev

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: {
     app: './src/index.js',
     print: './src/print.js'
@@ -11,6 +12,7 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname,'dist')
   },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
