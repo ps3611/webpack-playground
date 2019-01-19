39e88e5cf9ec996756872475ced81f8362590d20
Peter Schindler <petertasschindler@gmail.com>
2019-01-19 11:34:52 +0100

    feat: cleaning dist

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -23,6 +24,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({ title: 'Output Management' })
   ],
 }
