9a005a543f116acbae1e8772717f01eec03a3f6c
Peter Schindler <petertasschindler@gmail.com>
2019-01-19 11:20:24 +0100

    feat: adding new entry point

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,16 +1,23 @@
 import _ from 'lodash';
+import printMe from './print.js';
 import './style.css';
 import Icon from './icon.png';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
-  
+
   return element;
 }
 
